3dd9455594193d800b943de5d2d54ccb1f873ad5
Gonzalo <gonzalo.gonzalez@sg.gba.gob.ar>
2023-05-23 01:29:55 -0300

    crear projects

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {App} from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render('Hola mundo')
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+  <App />
+)
+/* <React.Fragment>
+    
+</React.Fragment> */
